f9390ffb9badcde06e3d3adc20e5f860cb172781
tcooper_mont <tomcooper@tec8.net>
2017-04-25 02:49:19 +0000

    initial checkin

---
+++ spec/exec_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../../server-inventory/lib/ruby/facter/packages.rb'
+require 'facter'
+
+describe "facter exec" do
+
+  before :each do
+    @os = Facter.value('operatingsystem')
+    @web = Facter.value('lamp_web')
+  end
+  describe "results" do
+    it "returns the correct os" do
+      expect(@os).to eq 'Ubuntu'
+    end
+  end
+  describe "web results" do
+    it "returns the correct os" do
+      expect(@web['name']).to eq 'apache2'
+    end
+  end
+end
