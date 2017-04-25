d8c0d85c8a1dcf6e59175f7c7612ecbe3b7d28e0
Tom Cooper <tomcooper@tec8.net>
2017-04-24 20:24:18 -0700

    commented out lamp_web

--- spec/exec_spec.rb
+++ spec/exec_spec.rb
@@ -1,10 +1,10 @@
-require_relative '../../server-inventory/lib/ruby/facter/packages.rb'
+#require_relative '../../server-inventory/lib/ruby/facter/packages.rb'
 require 'facter'
 
 describe "facter exec" do
 
   before :each do
-    @os = Facter.value('operatingsystem')
+    #@os = Facter.value('operatingsystem')
     @web = Facter.value('lamp_web')
   end
   describe "results" do
@@ -12,9 +12,9 @@ describe "facter exec" do
       expect(@os).to eq 'Ubuntu'
     end
   end
-  describe "web results" do
-    it "returns the correct os" do
-      expect(@web['name']).to eq 'apache2'
-    end
-  end
+  #describe "web results" do
+  #  it "returns the correct os" do
+  #    expect(@web['name']).to eq 'apache2'
+  #  end
+  #end
 end
